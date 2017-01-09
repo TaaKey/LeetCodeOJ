019211096b5de372840abe757bfc6f1495c4223c
wentaohong <hongwentao0705@gmail.com>
2017-01-09 11:50:08 -0500

    203. Remove Linked List Elements

---
+++ Util/ListNode.java
@@ -0,0 +1,13 @@
+package Util;
+
+/**
+ * Created by whong53 on 1/9/17.
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
