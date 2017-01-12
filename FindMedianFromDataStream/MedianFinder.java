f840dcb21ad27c4a49d72c987b221c29bd2f68ed
wentaohong <hongwentao0705@gmail.com>
2017-01-12 17:41:07 -0500

    295. Find Median from Data Stream

---
+++ FindMedianFromDataStream/MedianFinder.java
@@ -0,0 +1,38 @@
+package FindMedianFromDataStream;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+/**
+ * Created by whong53 on 1/12/17.
+ */
+
+public class MedianFinder {
+
+    PriorityQueue<Integer> minHeap;
+    PriorityQueue<Integer> maxHeap;
+
+    public MedianFinder() {
+        minHeap = new PriorityQueue<Integer>();
+        maxHeap = new PriorityQueue<Integer>(Collections.reverseOrder());
+    }
+
+    // Adds a number into the data structure.
+    public void addNum(int num) {
+        maxHeap.offer(num);
+        minHeap.offer(maxHeap.poll());
+        if (maxHeap.size() < minHeap.size()) {
+            maxHeap.offer(minHeap.poll());
+        }
+    }
+
+    // Returns the median of current data stream
+    public double findMedian() {
+        if (maxHeap.size() == minHeap.size()) {
+            return (double) (maxHeap.peek() + minHeap.peek()) / 2;
+        } else {
+            return maxHeap.peek();
+        }
+    }
+};
