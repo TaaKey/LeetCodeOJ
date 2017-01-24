8b66f45ddc89a0f5b4cd61a16e106aa61f2dbbed
wentaohong <hongwentao0705@gmail.com>
2017-01-23 20:30:51 -0500

    151. Reverse Words in a String

---
+++ ReverseWordsInAString/Solution.java
@@ -0,0 +1,16 @@
+package ReverseWordsInAString;
+
+/**
+ * Created by Wentao on 1/23/2017.
+ */
+public class Solution {
+    public String reverseWords(String s) {
+        String[] split = s.trim().split("\\s+");
+        StringBuilder sb = new StringBuilder();
+        for (int i = split.length - 1; i >= 0; i--) {
+            sb.append(split[i]);
+            sb.append(" ");
+        }
+        return sb.toString().trim();
+    }
+}
