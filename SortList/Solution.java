eecf338fc15bfdbf78e210ae82b4c200c3118af1
wentaohong <hongwentao0705@gmail.com>
2017-01-24 17:07:32 -0500

    148. Sort List

---
+++ SortList/Solution.java
@@ -0,0 +1,47 @@
+package SortList;
+
+import Util.ListNode;
+
+/**
+ * Created by whong53 on 1/24/17.
+ */
+public class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode prev = null, slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        // cut list in half
+        prev.next = null;
+        ListNode l1 = sortList(head);
+        ListNode l2 = sortList(slow);
+
+        return merge(l1, l2);
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode fakeHead = new ListNode(-1);
+        ListNode pointer = fakeHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                pointer.next = l1;
+                l1 = l1.next;
+            } else {
+                pointer.next = l2;
+                l2 = l2.next;
+            }
+            pointer = pointer.next;
+        }
+        if (l1 != null) {
+            pointer.next = l1;
+        } else {
+            pointer.next = l2;
+        }
+        return fakeHead.next;
+    }
+}
