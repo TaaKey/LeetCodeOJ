fbe9090e129a54ad8ea7a559d7d4bdc7a9205708
wentaohong <hongwentao0705@gmail.com>
2017-01-26 21:46:42 -0500

    242. Valid Anagram

--- ValidAnagram/Solution.java
+++ ValidAnagram/Solution.java
@@ -13,4 +13,20 @@ public class Solution {
         Arrays.sort(tChar);
         return String.valueOf(sChar).equals(String.valueOf(tChar));
     }
+
+    public boolean isAnagram2(String s, String t) {
+        int[] letters = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            letters[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            letters[t.charAt(i) - 'a']--;
+        }
+        for (int i : letters) {
+            if (i != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
