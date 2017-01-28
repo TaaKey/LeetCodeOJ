46be6e43fd6689246636d23e2ad7288d1974fa25
wentaohong <hongwentao0705@gmail.com>
2017-01-28 17:25:34 -0500

    287. Find the Duplicate Number

---
+++ FindTheDuplicateNumber/Solution.java
@@ -0,0 +1,41 @@
+package FindTheDuplicateNumber;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Wentao on 1/28/2017.
+ */
+public class Solution {
+    public int findDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        int result = 0;
+        for (int num : nums) {
+            if (!set.add(num)) {
+                result = num;
+                break;
+            }
+        }
+        return result;
+    }
+}
+
+public class Solution2 {
+    public int findDuplicate(int[] nums) {
+        int slow = 0;
+        int fast = 0;
+        while (true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if (slow == fast) {
+                break;
+            }
+        }
+        fast = 0;
+        while (fast != slow) {
+            fast = nums[fast];
+            slow = nums[slow];
+        }
+        return slow;
+    }
+}
