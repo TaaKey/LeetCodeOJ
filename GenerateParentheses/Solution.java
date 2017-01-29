5c9d98acfa80174ae667697a0996f557d114a8d7
wentaohong <hongwentao0705@gmail.com>
2017-01-28 22:21:54 -0500

    22. Generate Parentheses

---
+++ GenerateParentheses/Solution.java
@@ -0,0 +1,28 @@
+package GenerateParentheses;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Wentao on 1/28/2017.
+ */
+public class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, "", 0, 0, n);
+        return result;
+    }
+
+    private void backtrack(List<String> list, String str, int open, int close, int max) {
+        if (str.length() == max * 2) {
+            list.add(str);
+            return;
+        }
+        if (open < max) {
+            backtrack(list, str + "(", open + 1, close, max);
+        }
+        if (close < open) {
+            backtrack(list, str + ")", open, close + 1, max);
+        }
+    }
+}
