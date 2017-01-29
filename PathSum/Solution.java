8c057b91b0f3261267f6681bb4a6aab9ce6cc526
wentaohong <hongwentao0705@gmail.com>
2017-01-29 15:53:31 -0500

    112. Path Sum

---
+++ PathSum/Solution.java
@@ -0,0 +1,18 @@
+package PathSum;
+
+import Util.TreeNode;
+
+/**
+ * Created by Wentao on 1/29/2017.
+ */
+public class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.val == sum && root.left == null && root.right == null) {
+            return true;
+        }
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+}
