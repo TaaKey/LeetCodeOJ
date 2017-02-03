7c0844e34e4627d93cad60b100f462f9c0ac48e9
wentaohong <hongwentao0705@gmail.com>
2017-02-02 23:50:52 -0500

    485. Max Consecutive Ones

---
+++ MaxConsecutiveOnes/Solution.java
@@ -0,0 +1,19 @@
+package MaxConsecutiveOnes;
+
+/**
+ * Created by Wentao on 2/2/2017.
+ */
+public class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int result = 0, count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                count++;
+                result = Math.max(count, result);
+            } else {
+                count = 0;
+            }
+        }
+        return result;
+    }
+}
