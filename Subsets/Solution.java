00564c3403345091c23ea350c3eaec84924a01b7
wentaohong <hongwentao0705@gmail.com>
2017-02-02 23:41:47 -0500

    78. Subsets

---
+++ Subsets/Solution.java
@@ -0,0 +1,26 @@
+package Subsets;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Wentao on 2/2/2017.
+ */
+public class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(nums, result, new ArrayList<>(), 0);
+        return result;
+    }
+
+    private void backtrack(int[] nums, List<List<Integer>> result, List<Integer> item, int start) {
+        result.add(new ArrayList<>(item));
+        for (int i = start; i < nums.length; i++) {
+            item.add(nums[i]);
+            backtrack(nums, result, item, i + 1);
+            item.remove(item.size() - 1);
+        }
+    }
+}
