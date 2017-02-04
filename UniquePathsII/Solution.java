46aca846d9dd6782456039086aa44d289a5795da
wentaohong <hongwentao0705@gmail.com>
2017-02-04 13:50:09 -0500

    63. Unique Paths II

---
+++ UniquePathsII/Solution.java
@@ -0,0 +1,30 @@
+package UniquePathsII;
+
+/**
+ * Created by Wentao on 2/4/2017.
+ */
+public class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid.length == 0 || obstacleGrid[0].length == 0) {
+            return 0;
+        }
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    obstacleGrid[i][j] = 0;
+                } else if (i == 0 && j == 0) {
+                    obstacleGrid[i][j] = 1;
+                } else if (i == 0) {
+                    obstacleGrid[i][j] = obstacleGrid[i][j - 1];
+                } else if (j == 0) {
+                    obstacleGrid[i][j] = obstacleGrid[i - 1][j];
+                } else {
+                    obstacleGrid[i][j] = obstacleGrid[i][j - 1] + obstacleGrid[i - 1][j];
+                }
+            }
+        }
+        return obstacleGrid[m - 1][n - 1];
+    }
+}
