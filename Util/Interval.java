28df2bd6a91580a7b8604df0143aec5fefce1c58
wentaohong <hongwentao0705@gmail.com>
2017-02-09 22:50:56 -0500

    56. Merge Intervals

---
+++ Util/Interval.java
@@ -0,0 +1,19 @@
+package Util;
+
+/**
+ * Created by Wentao on 2/9/2017.
+ */
+public class Interval {
+    public int start;
+    public int end;
+
+    public Interval() {
+        start = 0;
+        end = 0;
+    }
+
+    public Interval(int s, int e) {
+        start = s;
+        end = e;
+    }
+}
