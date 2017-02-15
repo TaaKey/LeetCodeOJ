b03185f1e6b2715bb250adfac0c21169cfed3d70
wentaohong <hongwentao0705@gmail.com>
2017-02-14 22:40:44 -0500

    118. Pascal's Triangle

---
+++ PascalsTriangle/Solution.java
@@ -0,0 +1,22 @@
+package PascalsTriangle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Wentao on 2/14/2017.
+ */
+public class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> row = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            row.add(0, 1);
+            for (int j = 1; j < row.size() - 1; j++) {
+                row.set(j, row.get(j) + row.get(j + 1));
+            }
+            result.add(new ArrayList<>(row));
+        }
+        return result;
+    }
+}
