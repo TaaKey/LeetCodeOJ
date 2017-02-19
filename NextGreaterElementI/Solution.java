5cfd2465b9149495e2703ee59c3176108890d4ed
wentaohong <hongwentao0705@gmail.com>
2017-02-18 20:41:27 -0500

    496. Next Greater Element I

---
+++ NextGreaterElementI/Solution.java
@@ -0,0 +1,25 @@
+package NextGreaterElementI;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * Created by Wentao on 2/18/2017.
+ */
+public class Solution {
+    public int[] nextGreaterElement(int[] findNums, int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        for (int num : nums) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                map.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+        for (int i = 0; i < findNums.length; i++) {
+            findNums[i] = map.getOrDefault(findNums[i], -1);
+        }
+        return findNums;
+    }
+}
