a2f01166406b732f9ced01ebbc98817f915d74c6
wentaohong <hongwentao0705@gmail.com>
2017-03-01 15:00:38 -0500

    396. Rotate Function

---
+++ RotateFunction/Solution.java
@@ -0,0 +1,21 @@
+package RotateFunction;
+
+/**
+ * Created by whong53 on 3/1/17.
+ */
+public class Solution {
+    public int maxRotateFunction(int[] A) {
+        int sum = 0;
+        int F = 0;
+        for (int i = 0; i < A.length; i++) {
+            sum += A[i];
+            F += i * A[i];
+        }
+        int max = F;
+        for (int i = 1; i < A.length; i++) {
+            F = sum + F - A.length * A[A.length - i];
+            max = Math.max(max, F);
+        }
+        return max;
+    }
+}
